40d0748f3f76b65ee2531a4eeac304052117f23a
elbahaoui1 <aminelbahaoui@gmail.com>
2025-01-29 18:21:12 +0100

    leaderboard

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,7 +1,14 @@
-export default function Leaderboard() {
-    return(
-        <div className="">
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
-        </div>
-    )
+export default function LeaderBoard() {
+    return (<div className="h-screen text-white/80 w-full flex flex-col justify-center items-center text-4xl relative">
+        <Link href="/">
+            <Button variant="outline" className='absolute top-6 z-10 left-10 pl-3  rounded-full bg-transparent'>
+                <ArrowLeft />
+                Home</Button>
+        </Link>
+        Coming Soon!
+    </div>)
 }
